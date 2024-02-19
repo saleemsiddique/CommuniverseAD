451129940d2696add4bb25d7585fc3de8e27e9d0
saleemsiddique <saleemprogramacion04@gmail.com>
2024-02-19 19:19:39 +0100

    Initial commit

---
+++ communiverse/communiverse/src/main/java/com/example/communiverse/CommuniverseApplication.java
@@ -0,0 +1,13 @@
+package com.example.communiverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuniverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommuniverseApplication.class, args);
+	}
+
+}
