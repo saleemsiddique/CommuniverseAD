d5b0e28474186311690018ecddd5c41c3bc470c1
saleemsiddique <saleemprogramacion04@gmail.com>
2024-02-25 18:28:35 +0100

    Paso a spring-security 2.7.3

--- communiverse/communiverse/src/main/java/com/example/communiverse/payload/request/UserSignupRequest.java
+++ communiverse/communiverse/src/main/java/com/example/communiverse/payload/request/UserSignupRequest.java
@@ -5,9 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @AllArgsConstructor
 @NoArgsConstructor
