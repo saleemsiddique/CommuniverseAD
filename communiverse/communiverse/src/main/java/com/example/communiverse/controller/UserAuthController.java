d23ccb6bfb8f93d14f0b3fc0677d6d73fe5bde0b
saleemsiddique <saleemprogramacion04@gmail.com>
2024-02-28 21:01:24 +0100

    Login Error correction

--- communiverse/communiverse/src/main/java/com/example/communiverse/controller/UserAuthController.java
+++ communiverse/communiverse/src/main/java/com/example/communiverse/controller/UserAuthController.java
@@ -17,6 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -79,8 +80,13 @@ public class UserAuthController {
             return ResponseEntity
                     .badRequest()
                     .body("User not found with email or username: " + loginInput);
+        } catch (BadCredentialsException ex) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("Email, username, or password is incorrect");
         }
     }
+
 
 
     @PostMapping("/signup")
